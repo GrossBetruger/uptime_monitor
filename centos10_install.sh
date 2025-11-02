aac8e22f4acb9fc6cdf751a50682acaf701112fe
oren <oren.koriat@OrenKori-LM-IL.local>
2025-11-02 15:23:53 +0200

    feat: centos10

---
+++ centos10_install.sh
@@ -0,0 +1,9 @@
+sudo dnf -y install curl ca-certificates gcc gcc-c++ make pkgconf-pkg-config openssl-devel
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+echo 'source "$HOME/.cargo/env"' >> ~/.bashrc
+rustc --version
+cargo --version
+rustup component add rustfmt clippy rust-analyzer
+cargo new hello && cd hello
+cargo run
