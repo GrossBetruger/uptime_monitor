47243a36e5fa73584807de05235553aa1bd35457
oren <oren.koriat@OrenKori-LM-IL.local>
2025-11-17 13:00:20 +0200

    feat: build-for-everyone build mac releases

--- build_for_everybody.sh
+++ build_for_everybody.sh
@@ -1,4 +1,4 @@
-for user in $(cat users.csv); do export USER_NAME=$user; echo "Building for: $user" && ./build_for_windows.sh && mv target/x86_64-pc-windows-gnu/release/uptime_monitor.exe "uptime_monitor_$user.exe" && cargo build && cp target/debug/uptime_monitor monitor_$user && ./monitor_$user -t ; done
+for user in $(cat users.csv); do export USER_NAME=$user; echo "Building for: $user" && ./build_for_windows.sh && mv target/x86_64-pc-windows-gnu/release/uptime_monitor.exe "uptime_monitor_$user.exe" && cargo build --release && cp target/release/uptime_monitor monitor_$user && ./monitor_$user -t ; done
 
 sh build_for_centos.sh 
 sh extract_binary_centos.sh
