c3bd8a2f2fe2c4a6528533af6e63a42caafa6cd4
oren <oren.koriat@OrenKori-LM-IL.local>
2025-11-22 17:58:59 +0200

    feat: load test cmd

--- load_test.sh
+++ load_test.sh
@@ -1,6 +1,1 @@
-IP="$1"
-for i in $(seq 10); do
-  ./target/release/uptime_monitor 1 "http://$IP:3000/ingest" &
-  sleep 0.1
-done
-
+export USER_NAME=OrenK; cargo build --release; for i in $(seq 100); do target/release/uptime_monitor --url http://0.0.0.0:3000/ingest &; done
